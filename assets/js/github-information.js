724b90d2dc333d9282fde2903c9a0b5069e535cb
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-04-30 15:45:54 +0000

    create userInformationHTML function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,20 @@
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -8,22 +25,24 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img scr="assets/css/loader.gif" alt="loading . . ."/>
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
-    
+
     $.when(
-        $.getJSON(`https://api.github.com/users/{gh-username}`)
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
-        function(response){
+        function(response) {
             var userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponse) {
+        },
+        function(errorResponse) {
             if (errorResponse.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-        })
+        });
 }
