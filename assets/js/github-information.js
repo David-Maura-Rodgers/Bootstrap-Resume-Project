2e33a81cc8cf11d4c33de5f7f1ebfa3fe477c20e
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-04-30 15:23:35 +0000

    create fetchGitHubInformation function

---
+++ assets/js/github-information.js
@@ -0,0 +1,29 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img scr="assets/css/loader.gif" alt="loading . . ."/>
+        </div>`);
+    
+    $.when(
+        $.getJSON(`https://api.github.com/users/{gh-username}`)
+    ).then(
+        function(response){
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }, function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
+            }
+        })
+}
